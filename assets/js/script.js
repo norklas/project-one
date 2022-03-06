988974ee6dd862de9b740ad0e0fc32d36a42a641
Nick Melanson <nickm890@gmail.com>
2022-03-06 13:40:07 -0500

    modal functional, and campground data accessed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
 // API Keys
 var openWeatherApiKey = "bb50ceaa5c96b3d1b021c2c7a46d82b6";
-var campgroundApiKey = "s27xez64fvq76p9j724s632t";
+var campgroundApiKey = "Bmz374aWCDdIEBnTNdAjjyehjiMRkPJpJqGoY9qC";
+
+// Elements
+var searchFormEl = document.getElementById("search-form");
+var searchInput = document.getElementById("search-value");
+var modalEl = document.getElementById("popup-modal");
+var modalCloseBtn = document.getElementById("close-btn");
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+
+  var stateCode = searchInput.value.trim();
+
+  if (stateCode) {
+    campgroundData(stateCode);
+    searchInput.value = "";
+  } else {
+    modalEl.style.display = "block";
+  }
+}
+
+function campgroundData(stateCode) {
+  fetch(
+    `https://developer.nps.gov/api/v1/campgrounds?stateCode=${stateCode}&limit=4&api_key=${campgroundApiKey}`
+  ).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+}
+
+window.onclick = function (event) {
+  if (event.target == modalCloseBtn) {
+    modalEl.style.display = "none";
+  }
+};
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
