7590391027d6c791980823a222f2471726045f63
Nick Melanson <nickm890@gmail.com>
2022-03-10 17:46:17 -0500

    css functional but NOT responsive yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,15 +35,15 @@ function formSubmitHandler(e) {
     campgroundData(stateCode);
     searchInput.val("");
   } else {
-    modalErrorText.textContent = "Please enter correct state abbreviation!";
-    modalEl.style.display = "block";
+    $(modalErrorText).text("Please enter correct state abbreviation!");
+    $(modalEl).css("display", "block");
   }
 }
 
 // Function to fetch campground data
 function campgroundData(stateCode) {
   fetch(
-    `https://developer.nps.gov/api/v1/campgrounds?stateCode=${stateCode}&limit=4&api_key=${campgroundApiKey}`
+    `https://developer.nps.gov/api/v1/campgrounds?stateCode=${stateCode}&limit=6&api_key=${campgroundApiKey}`
   ).then(function (response) {
     if (response.ok) {
       return response.json().then(function (data) {
@@ -51,8 +51,8 @@ function campgroundData(stateCode) {
         displayCampgroundInfo(data, stateCode);
       });
     } else {
-      modalErrorText.textContent = "No campground data found";
-      modalEl.style.display = "block";
+      $(modalErrorText).text("No campground data found");
+      $(modalEl).css("display", "block");
     }
   });
 }
@@ -62,74 +62,73 @@ function displayCampgroundInfo(campgrounds) {
   console.log(campgrounds);
 
   // Clear old content
-  //cardSection.html("");
+  $(campCards).html("");
 
   // Loop over campgrounds
   for (var i = 0; i < campgrounds.data.length; i++) {
     // Articles for camp cards
     var campInfo = $("<article>");
     campCards.append(campInfo);
+    campInfo.addClass(
+      "max-w-sm rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700 my-4 hover:opacity-90"
+    );
+    campInfo.attr("id", "card-info");
 
     // Creating image element, grabbing image url through data array, setting size, and appending it
+    var imgDiv = $("<div>");
     var imgEl = $("<img>");
     imgEl.attr("src", campgrounds.data[i].images[0].url);
-    imgEl.addClass("object-cover h-60 w-60");
+    imgEl.addClass("rounded-t-lg h-100 w-100 object-fill");
 
-    campInfo.append(imgEl);
+    campInfo.append(imgDiv);
+    imgDiv.append(imgEl);
+
+    // Creating title element, and appending it to camp info article within card container
+    var infoDiv = $("<div>");
+    campInfo.append(infoDiv);
+    infoDiv.addClass("p-5");
 
     // Campground name
     var campgroundName = campgrounds.data[i].name;
 
-    // Creating title element, and appending it to camp info article within card container
-    var titleEl = $("<span>");
+    var titleEl = $("<h5>");
+    titleEl.addClass(
+      "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    );
     titleEl.append(campgroundName);
-
-    campInfo.append(titleEl);
+    infoDiv.append(titleEl);
 
     // Creating p elements for address
     var descriptionEl = $("<p>");
     descriptionEl.text("Description: " + campgrounds.data[i].description);
+    descriptionEl.addClass("mb-3 font-normal text-gray-700 dark:text-gray-400");
 
     var campsitesEl = $("<p>");
+    campsitesEl.addClass("font-semibold");
     campsitesEl.text(
       "Total sites: " + campgrounds.data[i].campsites.totalSites
     );
 
-    var operatingDescriptionEl = $("<p>");
-    operatingDescriptionEl.text(
-      "Operating description: " +
-        campgrounds.data[i].operatingHours[0].description
-    );
-
-    var operatingHoursListEl = $("<ul>");
-
-    campInfo.append(operatingDescriptionEl);
-    campInfo.append(operatingHoursListEl);
-    campInfo.append(descriptionEl);
-    campInfo.append(campsitesEl);
+    //infoDiv.append(descriptionEl);
+    infoDiv.append(campsitesEl);
 
     // Creating p element for cost
     var costEl = $("<p>");
+    costEl.addClass("font-semibold");
     costEl.text("Cost: $" + campgrounds.data[i].fees[0].cost + " per night");
 
-    campInfo.append(costEl);
-
-    // Creating phone number element
-    var phoneEl = $("<a>");
-    phoneEl.attr(
-      "href",
-      campgrounds.data[i].contacts.phoneNumbers[0].phoneNumber
-    );
-    phoneEl.text(campgrounds.data[i].contacts.phoneNumbers[0].phoneNumber);
-
-    campInfo.append(phoneEl);
+    infoDiv.append(costEl);
 
     // Creating anchor element for more info
     var linkEl = $("<a>");
     linkEl.attr("href", campgrounds.data[i].url);
-    linkEl.text("View more info");
+    linkEl.attr("id", "card-btn");
+    linkEl.addClass(
+      "inline-flex items-center py-2 px-3 mt-4 text-sm font-medium text-center rounded-lg focus:ring-4 font-bold"
+    );
+    linkEl.text("Read more");
 
-    campInfo.append(linkEl);
+    infoDiv.append(linkEl);
   }
 }
 
@@ -149,6 +148,8 @@ function loadlastState() {
 
 const liMaker = (stateCode) => {
   const li = $("<li>");
+  li.addClass("ml-2 mt-4 w-48 text-center rounded-lg");
+  li.attr("id", "list-item");
   const ul = $("<ul>");
   li.text(stateCode);
   ul.append(li);
@@ -162,7 +163,8 @@ data.forEach((stateCode) => {
 clearBtn.on("click", function () {
   localStorage.clear();
   $("ul").empty();
-  itemsArray = [];
+  //itemsArray = [];
+  document.location.reload();
 });
 
 //Dynamically add the passed state on the search history
@@ -187,11 +189,9 @@ function clearHistory(event) {
 }
 
 // Function to close modal
-window.onclick = function (event) {
-  if (event.target == modalCloseBtn) {
-    modalEl.style.display = "none";
-  }
-};
+$(modalEl).on("click", "button", function () {
+  $(modalEl).css("display", "none");
+});
 
 searchFormEl.on("submit", formSubmitHandler);
 loadlastState();
